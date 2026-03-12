e220c8c54e2190e3d41ac8415b44ce9d57e8d799
martinbergeron <marxtinus@gmail.com>
2026-03-11 22:38:03 -0400

    under construction

---
+++ resources/js/Pages/UnderConstruction.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center p-6 overflow-auto">
+        <img
+            src="/img/en%20construction"
+            alt="Under construction"
+            class="block h-[80vh] object-contain shadow-2xl"
+        />
+    </div>
+</template>
+
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/un', function () {
+    return Inertia::render('UnderConstruction');
+})->name('underconstruction');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
