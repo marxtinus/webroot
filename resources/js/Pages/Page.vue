78af2cd6822d236d64a33b5b5c7ba1222ea7a675
martinbergeron <marxtinus@gmail.com>
2026-05-07 19:26:51 -0400

    Version 3

--- resources/js/Pages/Page.vue
+++ resources/js/Pages/Page.vue
@@ -21,7 +21,7 @@
                     <p class="text-gray-500 text-sm italic">Développeur Web Automatisations et Rapports</p>
                     <hr class="my-2 border-gray-200" />
                     <p class="text-gray-500 text-sm">Veuillez noter que le présent portfolio est en cours de développement.</p>
-                    <p class="text-gray-500 text-sm">Je vous prie de ne pas juger trop sévèrement, il s’agit d’un travail inachevé</p>
+                    <p class="text-gray-500 text-sm">Je vous prie de ne pas juger trop sévèrement, il s’agit d’un travail inachevé.</p>
 
 
                     <hr class="my-2 border-gray-200" />
